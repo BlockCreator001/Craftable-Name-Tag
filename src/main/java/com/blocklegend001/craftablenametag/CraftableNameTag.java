29a4fe8845690ca9cd9843e0df1e71f23a20ef46
BlockCreator001 <62543902+BlockCreator001@users.noreply.github.com>
2022-07-09 17:42:00 +0200

    Add files via upload

---
+++ src/main/java/com/blocklegend001/craftablenametag/CraftableNameTag.java
@@ -0,0 +1,36 @@
+package com.blocklegend001.craftablenametag;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(CraftableNameTag.MOD_ID)
+public class CraftableNameTag
+{
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public static final String MOD_ID = "craftablenametag";
+
+    public CraftableNameTag() {
+
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        eventBus.addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+        LOGGER.info("HELLO FROM PREINIT");
+
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
